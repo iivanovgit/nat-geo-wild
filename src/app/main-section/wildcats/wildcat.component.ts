2761355338b8060c6895d9ef464982bc0644e709
iivanovgit <iivanov.reg@gmail.com>
2016-08-22 04:06:33 +0300

    remove spec.ts

--- src/app/main-section/wildcats/wildcat.component.ts
+++ src/app/main-section/wildcats/wildcat.component.ts
@@ -43,10 +43,3 @@ export class WildcatComponent implements OnInit, OnDestroy {
     this.router.navigate(['/wildcats', { id: wildcatId, foo: 'foo' }]);
   }
 }
-
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
